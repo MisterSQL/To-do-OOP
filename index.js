6c62e61228733c9bb6ce62394b5f5fd53236c2cb
Suren Poghosyan <dendy20123@gmail.com>
2023-08-25 12:13:22 -0700

    another_way_create_to_do

--- index.js
+++ index.js
@@ -1,63 +1,39 @@
-class Product{
-    constructor(containerNotes, name,numberProducts) {
-
-        // note
-        this.note = document.createElement("div")
-        this.note.classList.add("note")
-
-        // count Products
-        this.count = document.createElement("div")
-        this.count.innerText = numberProducts;
-        this.count.classList.add("countProducts");
-
-        // name Product
-        this.nameProduct = document.createElement("span");
-        this.nameProduct.classList.add("nameProducts")
-        this.nameProduct.innerText = name
-
-        // button done
-        this.buttonDone = document.createElement("input")
-        this.buttonDone.type = "checkbox"
-        this.buttonDone.classList.add("buttonDone")
-        this.buttonDone.addEventListener('click',()=>{
-                if(this.buttonDone.checked){
-                    this.note.classList.add("bg-dark");
-                }
-                else {
-                    this.note.classList.remove("bg-dark");
-                }
-        })
-
-        // button delete
-        this.buttonDelete = document.createElement("button");
-        this.buttonDelete.classList.add("buttonsDelete")
-        this.buttonDelete.innerText = "X";
-        this.buttonDelete.addEventListener('click',()=>{
-            this.note.remove()
-        })
-
-        // Container for Buttons
-        this.containerButtons = document.createElement("div");
-        this.containerButtons.classList.add("containerButtons");
-
-        // Add buttons in container
-        this.containerButtons.append(this.buttonDone, this.buttonDelete)
-
-        // Add information about note
-        this.note.append(this.count,this.nameProduct,this.containerButtons);
-
-        // Add note in List
-        containerNotes.append(this.note)
-    }
-
+let index = 0;
+const containerNotes = document.getElementById("listProducts");
+function createNote(containerNotes,name,count){
+    containerNotes.innerHTML += getNote(name,count);
 }
 
-const containerNotes = document.getElementById("listProducts");
+function getNote(name,count){
+    return`
+           <div class="note">
+                <div class="countProducts">
+                    ${count.value}
+                </div>
+                <span class="nameProducts">${name.value}</span>
+                <div class="containerButtons">
+                    <input class="buttonDone" type="checkbox" onclick="statusNote(this,this.parentNode)" value=${index++}>
+                    <button class="buttonsDelete" onclick="removeNote(this.parentNode)">X</button>
+                </div>
+           </div>
+        `
+}
+
+function removeNote(parent){
+    parent.parentNode.remove();
+}
+
+function statusNote(statusNote,containerNote){
+    if(statusNote.checked){
+        containerNote.parentNode.classList.add("bg-dark")
+    }
+    else{
+        containerNote.parentNode.classList.remove("bg-dark")
+    }
+}
 
 function addProduct(){
     const nameProduct = document.getElementById("nameProduct")
     const countProducts = document.getElementById("countProducts");
-    new Product(containerNotes,nameProduct.value,countProducts.value)
+    createNote(containerNotes,nameProduct,countProducts)
 }
-
-
